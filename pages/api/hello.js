1627d38e3176f1bcbbd939083bb707bcebfcab6a
Marcos <marcosnery.comp@gmail.com>
2021-05-17 00:58:59 -0300

    feat: adding log function

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const fs = require("fs");
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  const data = req.query;
+  const timestamp = new Date().toISOString();
+  const logInfo = `${timestamp},${data.device},${data.action}\n`;
+
+  fs.writeFile("log.csv", logInfo, { flag: "a" }, function (err) {
+    if (err) return console.log(err);
+  });
+
+  res.send(`Adicionado ao log: ${JSON.stringify(data)}`);
+};
